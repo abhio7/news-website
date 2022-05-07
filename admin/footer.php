65fd505e54a5af6dbb033d62dcda04864b01565a
abhio7 <89398489+abhio7@users.noreply.github.com>
2022-05-08 01:38:12 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 20** News | Powered by News</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+
+</html>
